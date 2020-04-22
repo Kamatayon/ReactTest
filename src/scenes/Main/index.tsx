1c2d9387a1b4c62d9d605ba1a1cb79fa86bd3ac4
Kamatayon <wowegable@gmail.com>
2020-04-22 17:47:50 +0800

    Header done, added assets, basic folder structure

---
+++ src/scenes/Main/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+import PageHeader from "components/Header";
+export const SiteWrapper = () => {
+  return (
+    <div id="site-wrapper">
+      <PageHeader />
+    </div>
+  );
+};
+
+export default SiteWrapper;
